d6b5c85a834b08c72dfb2e4232e0aa46c09163d0
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-03-30 23:44:03 -0400

    Add graphs

--- DataDashboard/src/Components/List.jsx
+++ DataDashboard/src/Components/List.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
-export default function List({inputs}){
-    const [pastWeather, setPastWeather] = useState([{}]);
+export default function List({inputs, pastWeather, setPastWeather}){
+
     useEffect(() => {
         const getWeatherHist = async () => {
         const response = await fetch(`https://api.weatherbit.io/v2.0/history/daily?city=NY&start_date=${inputs.startDate}&end_date=${inputs.endDate}&key=${API_KEY}`)
