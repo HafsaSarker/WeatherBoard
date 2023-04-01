9b240b1b283c06e27db9aa7ee59ece03e58fcde2
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-03-31 21:47:31 -0400

    create weather table

--- DataDashboard/src/Components/WeatherDetails.jsx
+++ DataDashboard/src/Components/WeatherDetails.jsx
@@ -14,14 +14,64 @@ const WeatherDetails = () => {
         const getWeatherHist = async () => {
         const response = await fetch(`https://api.weatherbit.io/v2.0/history/daily?city=NY&start_date=${params.date}&end_date=${end}&key=${API_KEY}`)
         const json = await response.json()
-        setFullDetails(json.data);
+        setFullDetails(json);
         }
         getWeatherHist().catch(console.error);
     },[])
-    console.log(fullDetails)
+
     return (
      <div>
-        deetscompo
+        { fullDetails ? (
+            <div>
+                <h1>{fullDetails.city_name}</h1>
+                <h2>Date: {fullDetails.data[0].datetime}</h2>
+                <h4>Lat: {fullDetails.lat}, Lon: {fullDetails.lon}</h4>
+
+                <table className="weatherDetails">
+                    <tbody>
+                        <tr>
+                            <th>Average temperature</th>
+                            <td>{fullDetails.data[0].temp} °C</td>
+                        </tr>
+                        <tr>
+                            <th>Average cloud coverage</th>
+                            <td>{fullDetails.data[0].clouds} %</td>
+                        </tr>
+                        <tr>
+                            <th>High</th>
+                            <td>{fullDetails.data[0].max_temp} °C</td>
+                        </tr>
+                        <tr>
+                            <th>Low</th>
+                            <td>{fullDetails.data[0].min_temp} °C</td>
+                        </tr>
+                        <tr>
+                            <th>Average pressure</th>
+                            <td>{fullDetails.data[0].pres} mb</td>
+                        </tr>
+                        <tr>
+                            <th>Average wind speed</th>
+                            <td>{fullDetails.data[0].wind_spd} m/s</td>
+                        </tr>
+                        <tr>
+                            <th> Average relative humidity</th>
+                            <td>{fullDetails.data[0].rh} %</td>
+                        </tr>
+                        <tr>
+                            <th>Average solar radiation</th>
+                            <td>{fullDetails.data[0].solar_rad} W/M^2</td>
+                        </tr>
+                        <tr>
+                            <th>Accumulated precipitation</th>
+                            <td>{fullDetails.data[0].precip_gpm} mm</td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+
+            </div>
+
+        ) : null }
      </div>
     );
 };
